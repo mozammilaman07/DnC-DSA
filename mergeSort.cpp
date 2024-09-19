d1fbff25d644b60413912ff614f468f73ddfdcd0
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-19 23:52:54 +0530

    printing the array

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -9,4 +9,10 @@ int main()
 {
     int arr[] = {4, 45, 84, 2, 56, 49, 21, 6, 52, 43};
     int size = 10;
+    mergeSort(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
